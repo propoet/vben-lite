8e41b4fd70805c89851db1b5c1e82b3c41abc045
zhangcm <propoet@163.com>
2026-01-19 11:52:00 +0800

    验证暗色模式基础支持

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,30 @@
 <script setup lang="ts">
-  import { APP_NAME } from '#/utils'
-  console.log(APP_NAME)
+  import { useDark,useToggle } from '@vueuse/core';
+
+  // const isDark = ref(false)
+  // const toggleDark = () => {
+  //   isDark.value = !isDark.value
+  //   document.documentElement.classList.toggle('dark', isDark.value)
+  // }
+  const isDark = useDark({
+  storageKey: 'theme',
+})
+
+const toggleDark = useToggle(isDark)
+
 </script>
 
 <template>
-  <div class="app">
-    <h1 class="text-3xl font-bold underline">Vben Lite</h1>
+ <div
+    class="min-h-screen bg-white text-gray-900 dark:bg-gray-900 dark:text-white p-4"
+  >
+    <h1 class="text-2xl font-bold">Vben Lite </h1>
+    <button
+      class="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
+      @click="toggleDark()"
+    >
+      切换主题: {{ isDark ? "暗色" : "亮色" }}
+    </button>
   </div>
 </template>
 
