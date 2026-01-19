1caadfbf2a4fa8b407c2bd9f6a53068994178d40
zhangcm <propoet@163.com>
2026-01-19 13:41:01 +0800

    创建项目常量

---
+++ src/constants/index.ts
@@ -0,0 +1,10 @@
+/**登录页面路径 */
+export const LOGIN_PATH = '/auth/login'
+/** 默认首页路径 */
+export const DEFAULT_HOME_PATH = "/dashboard";
+
+/** Token 存储键名 */
+export const ACCESS_TOKEN_KEY = "access_token";
+
+/** 偏好设置存储键名 */
+export const PREFERENCES_KEY = "preferences";
