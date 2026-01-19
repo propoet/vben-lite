80a0f5cfe2d0d34b0ae14a7e5013e842d088ef14
zhangcm <propoet@163.com>
2026-01-19 14:46:59 +0800

    常量定义, 提示词优化

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,10 +1,2 @@
-/**登录页面路径 */
-export const LOGIN_PATH = '/auth/login'
-/** 默认首页路径 */
-export const DEFAULT_HOME_PATH = "/dashboard";
-
-/** Token 存储键名 */
-export const ACCESS_TOKEN_KEY = "access_token";
-
-/** 偏好设置存储键名 */
-export const PREFERENCES_KEY = "preferences";
+export * from './core';
+export * from '#/shared/constants';
